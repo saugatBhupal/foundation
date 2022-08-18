c91d1a194c1e418cc321691f53b990e30bfb7f1e
saugat singh <saugatbhupal@bhunet.com>
2022-08-18 09:48:39 +0545

    edit

--- Documents/foundation/classwork-1.py
+++ Documents/foundation/classwork-1.py
@@ -1,5 +1,5 @@
 def sendName(firstName, lastName):
-    return (firstName + " " + lastName)
+    return (firstName +" " + lastName)
 firstName = input("Enter First Name:")
 secondName = input("Enter Second Name:")
-print("Your full name is: " + sendName(firstName,secondName))
+print("Your full name is: " , sendName(firstName,secondName))
